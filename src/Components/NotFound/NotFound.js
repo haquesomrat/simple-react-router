ceb5b2ac714b3f44a3ef098e6add2d3e39f1958e
Injamul Haque <ihsomrat.official@gmail.com>
2021-10-28 20:41:38 +0600

    first

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>This page is not available</h3>
+            <p>Sorry!! I do not know abou this</p>
+        </div>
+    );
+};
+
+export default NotFound;
